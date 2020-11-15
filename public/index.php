63556838e73721aaca952a20b9376f078a3e7ced
mhewitt9pq <73035012+mhewitt9pq@users.noreply.github.com>
2020-11-15 14:44:56 +0000

    Added form and radio and submit buttons

--- public/index.php
+++ public/index.php
@@ -8,7 +8,17 @@ include_once 'header.php';
     <div class="row">
             <div class="card mt-3 px-2 py-2">
                 <h1>Stationary Application</h1>
-                <p1>Welcome to this fictional stationary application page created for COMP2001 example. To continue, please accept our terms and conditions.</p1>
+                <p1>Welcome to this fictional stationary application page created for COMP2001 example. To continue, please accept our terms and conditions.</p1><br>
+
+                <form>
+                    <input type="radio" id="I agree" name="agreement" value="I agree">
+                    <label for="I agree">I agree</label><br>
+                    <input type="radio" id="I disagree" name="agreement" value="I disagree">
+                    <label for="I disagree">I disagree</label><br><br>
+
+
+                    <input type="submit" value="Submit">
+                </form>
             </div>
     </div>
     </div>
