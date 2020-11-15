31b90475ed93938690580988a51cfc1f175ba7f7
mhewitt9pq <73035012+mhewitt9pq@users.noreply.github.com>
2020-11-15 14:10:59 +0000

    Edited index

--- public/index.php
+++ public/index.php
@@ -1,8 +1,18 @@
 <?php
-include_once ‘public/header.php‘;
+include_once 'header.php';
 
 ?>
 
+<body class="bginfo">
+    <div class="container-fluid col-md-10 offset-md-1">
+    <div class="row">
+            <div class="card mt-3 px-2 py-2">
+                <h1>Stationary Application</h1>
+                <p1>Welcome to this fictional stationary application page created for COMP2001 example. To continue, please accept our terms and conditions.</p1>
+            </div>
+    </div>
+    </div>
+</body>
 
 
 
@@ -16,6 +26,5 @@ include_once ‘public/header.php‘;
 
 
 
-
-<?php include_once ‘public/footer.php‘; ?>
+<?php include_once 'footer.php'; ?>
 
